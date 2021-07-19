6c16e6d7262e2f8c08914d5dc000eb54ce98a593
lonkaut@gmail.com <Cal$4p0rts>
2021-07-19 13:21:02 -0500

    Fixed fingerprint import

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import git
 import subprocess
 from time import sleep
+from os import environ
 
-subprocess.run(["/usr/bin/ssh-keyscan", "github.com", ">>", "~/.ssh/known_hosts"], shell=True)
+
+# subprocess.run(["/usr/bin/ssh-keyscan", "github.com", ">>", "~/.ssh/known_hosts"], shell=True)
+HOME = environ['HOME']
+print(f"{HOME}/.ssh/known_hosts")
+
+f = open(f"{HOME}/.ssh/known_hosts", "a")
+subprocess.call(["/usr/bin/ssh-keyscan", "github.com"], stdout=f)
 
 while True:
     g = git.cmd.Git("/project")
