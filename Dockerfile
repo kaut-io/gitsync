14fc5a4bea7de78e8ca7b2c7a9d6d0b9f5774ab3
lonkaut@gmail.com <Cal$4p0rts>
2021-07-19 12:51:18 -0500

    Added more to the Python script Changed Dockerfile to add the python scirpt

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,6 @@ RUN apk update && \
 apk add python3 py3-pip git bash curl openssh-client && \
 pip3 install gitpython
 
-CMD ["/usr/bin/python3", "main.py"]
+ADD main.py /
+
+CMD ["/usr/bin/python3", "/main.py"]
--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import git
 import subprocess
 from time import sleep
 
-subprocess.run(["/usr/bin/ssh-keyscan" "github.com" ">>" "~/.ssh/known_hosts"])
+subprocess.run(["/usr/bin/ssh-keyscan", "github.com", ">>", "~/.ssh/known_hosts"])
 
 while True:
+    g = git.cmd.Git("/project")
+    g.pull()
     sleep(300)
