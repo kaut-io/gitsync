1a6116b7ee9cc8e5013056f718fdfe0947519112
lonkaut@gmail.com <Cal$4p0rts>
2021-07-19 12:58:44 -0500

    Fixed subprocess call

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import git
 import subprocess
 from time import sleep
 
-subprocess.run(["/usr/bin/ssh-keyscan", "github.com", ">>", "~/.ssh/known_hosts"])
+subprocess.run(["/usr/bin/ssh-keyscan", "github.com", ">>", "~/.ssh/known_hosts"], shell=True)
 
 while True:
     g = git.cmd.Git("/project")
